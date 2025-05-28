95834de130845d7e83336343737d40c204d6a666
m67pg <m_owaki@nifty.com>
2025-05-28 12:14:19 +0900

    no message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -34,9 +34,7 @@ class MessageController extends Controller
             'created_at' => now(),
         ]);
 
-        //return response()->json($message->load('user'));
         return new MessageResource($message->load('user'));
-
     }
 
     /**
